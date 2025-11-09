f9ef3dacb9e2eac4877c233430e61feaed6d5358
Rangel <96200914+rangelbjordao@users.noreply.github.com>
2025-11-09 13:22:44 -0300

    remocao do token bearer pra testes

--- src/services/spotifyService.ts
+++ src/services/spotifyService.ts
@@ -2,13 +2,10 @@ import api from "@/services/api";
 import { Musica } from "@/types/spotify";
 
 export const SpotifyService = {
-  obterUrlLoginSpotify: async (tokenJwt: string): Promise<string> => {
+  obterUrlLoginSpotify: async (): Promise<string> => {
     try {
       const resposta = await api.get("/spotify/auth", {
-        headers: {
-          Authorization:
-            "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0ZUBlbWFpbC5jb20iLCJpc3MiOiJBUEkgRW1vdGlXYXZlIiwiZXhwIjoxNzYyNjEzNzU0fQ.3Bubob7g6Y7W8QXNjNzouRP75nyRNJgYVqxg4asB_8I",
-        },
+        headers: {},
       });
 
       const urlCorrigida = resposta.data.replace("127.0.0.1", "192.168.15.58");
